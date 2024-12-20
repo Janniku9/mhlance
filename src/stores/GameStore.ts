8efa1b092c956418deafaa886782453bef5b26a6
Janniku9 <jannikg@student.ethz.ch>
2024-12-20 23:57:48 +0100

    updated game store

--- src/stores/GameStore.ts
+++ src/stores/GameStore.ts
@@ -2,3 +2,8 @@ import { atom } from 'nanostores';
 import type { GameType } from '../types/Game';
 
 export const currentGameId = atom<GameType>(null);
+
+export const gameStore = {
+  setGame: (id: GameType) => currentGameId.set(id),
+  clearGame: () => currentGameId.set(null),
+};
