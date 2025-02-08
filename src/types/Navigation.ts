a63d90ec0617b5fbbc3a0ee725877e2c84f7852d
Janniku9 <jannik@nunu.ai>
2025-02-08 19:33:56 +0100

    new menu dropdown with emojis

--- src/types/Navigation.ts
+++ src/types/Navigation.ts
@@ -3,6 +3,7 @@ import type { Game } from './Game';
 export interface NavItem {
   id: string;
   title: string;
+  emoji?: string;
   description?: string;
 }
 
@@ -26,26 +27,31 @@ export const games: GameNavigation[] = [
         {
           id: 'low-rank',
           title: 'Low Rank',
+          emoji: '🌋',
           description: 'Low rank progression builds',
         },
         {
           id: 'high-rank',
           title: 'High Rank',
+          emoji: '💠',
           description: 'High rank progression builds',
         },
         {
           id: 'master-rank',
           title: 'Master Rank',
+          emoji: '❄️',
           description: 'Master rank pr  ogression builds',
         },
         {
           id: 'post-story',
           title: 'Post-Story',
+          emoji: '🐲',
           description: 'Post story & guiding lands builds',
         },
         {
           id: 'endgame',
           title: 'Endgame',
+          emoji: '🐉',
           description: 'Fatalis endgame builds',
         },
       ],
@@ -53,31 +59,37 @@ export const games: GameNavigation[] = [
         {
           id: 'combos',
           title: 'Combos',
+          emoji: '🥊',
           description: 'Optimal combos for all situations',
         },
         {
           id: 'guarding',
           title: 'Guard Mechanics',
+          emoji: '🛡️',
           description: 'Detailed explanation of guard mechanics',
         },
         {
           id: 'clutch-claw',
           title: 'Clutch Claw',
+          emoji: '⚓',
           description: 'Detailed explanation of clutch claw',
         },
         {
           id: 'mantles',
           title: 'Mantles',
+          emoji: '🧥',
           description: 'Detailed explanation of mantles',
         },
         {
           id: 'palico',
           title: 'Palico',
+          emoji: '🐈',
           description: 'Best palico equipment and skills',
         },
         {
           id: 'food-skills',
           title: 'Food Skills',
+          emoji: '🥩',
           description: 'Best food skills for all situations',
         },
       ],
@@ -86,11 +98,13 @@ export const games: GameNavigation[] = [
         {
           id: 'guard-up',
           title: 'Guard Up',
+          emoji: '🛡️',
           description: 'Guard up values for all situations',
         },
         {
           id: 'guiding-lands',
           title: 'Guiding Lands',
+          emoji: '🏞️',
           description: 'Detailed explanation of guiding lands',
         },
       ],
@@ -104,26 +118,31 @@ export const games: GameNavigation[] = [
         {
           id: 'low-rank',
           title: 'Low Rank',
+          emoji: '🏯',
           description: 'Low rank progression builds',
         },
         {
           id: 'high-rank',
           title: 'High Rank',
+          emoji: '🌄',
           description: 'High rank progression builds',
         },
         {
           id: 'master-rank',
           title: 'Master Rank',
+          emoji: '🏰',
           description: 'Master rank progression builds',
         },
         {
           id: 'post-story',
           title: 'Post-Story',
+          emoji: '🩸',
           description: 'Post story & anomaly progression builds',
         },
         {
           id: 'endgame',
           title: 'Endgame',
+          emoji: '👑',
           description: 'Endgame meta builds',
         },
       ],
@@ -131,26 +150,31 @@ export const games: GameNavigation[] = [
         {
           id: 'combos',
           title: 'Combos',
+          emoji: '🥊',
           description: 'Optimal combos for all situations',
         },
         {
           id: 'switch-skills',
           title: 'Switch Skills',
+          emoji: '🔄',
           description: 'Detailed explanation of switch skills',
         },
         {
           id: 'palamutes',
           title: 'Palamutes',
+          emoji: '🐕‍',
           description: 'Best palamute equipment and skills',
         },
         {
           id: 'dango',
           title: 'Dango',
+          emoji: '🍡',
           description: 'Best dango combos for all situations',
         },
         {
           id: 'followers',
           title: 'Followers',
+          emoji: '📯‍',
           description: 'Best followers combos for solo hunting',
         },
       ],
@@ -159,21 +183,25 @@ export const games: GameNavigation[] = [
         {
           id: 'match-ups',
           title: 'Match Ups',
+          emoji: '📝',
           description: 'Full match up chart for all monsters',
         },
         {
           id: 'guard-up',
           title: 'Guard Up',
+          emoji: '🛡️',
           description: 'Recommended guard up values for all monsters',
         },
         {
           id: 'anomaly-materials',
           title: 'Anomaly Materials',
+          emoji: '🦴',
           description: 'Anomaly material drop tables',
         },
         {
           id: 'anti-species',
           title: 'Anti-Species',
+          emoji: '🧬',
           description: 'Anti species decoration match ups',
         },
       ],
--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,7 @@ export default {
         },
         background: {
           DEFAULT: '#1e181c',
-          dark: '##191418',
+          dark: '#191418',
           darker: '#141014',
         },
       },
