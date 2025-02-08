bce05306a09679232b2f50aa3b9ae2bad00379e6
Janniku9 <jannik@nunu.ai>
2025-02-09 00:08:43 +0100

    finished landing page

---
+++ src/data/Socials.ts
@@ -0,0 +1,16 @@
+import type { Social } from '../types/Socials';
+
+export const socials: Social[] = [
+  {
+    name: 'GitHub',
+    description: 'Contribute today!',
+    url: 'https://github.com/j9-mh/j9mh',
+    icon: 'mingcute:github-fill',
+  },
+  {
+    name: 'Discord',
+    description: 'Come discuss with us',
+    url: 'https://discord.gg/monsterhunter',
+    icon: 'mingcute:discord-fill',
+  },
+];
