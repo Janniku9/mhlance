f72717aa7dd81d7c872dc9acc12157c162f83907
Janniku9 <39667156+Janniku9@users.noreply.github.com>
2025-02-16 18:27:25 +0100

    wip switch skills

--- src/directives/fade-in.ts
+++ src/directives/fade-in.ts
@@ -11,7 +11,7 @@ export default function fadeInDirective(element: HTMLElement) {
       });
     },
     {
-      rootMargin: window.innerWidth < 768 ? '0px 0px -100px 0px' : '0px 0px -200px 0px',
+      rootMargin: window.innerWidth < 768 ? '0px 0px -100px 0px' : '0px 0px -400px 0px',
       threshold: 0,
     }
   );
