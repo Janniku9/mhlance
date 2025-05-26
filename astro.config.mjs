bd1f91bb7ac0e5c5692c6bc230bc44ab054180f1
jannik <gartmann.jannik@gmail.com>
2025-05-26 22:11:05 +0000

    experimental page find

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()],
+  integrations: [icon(), tailwind(), pagefind()],
 });
