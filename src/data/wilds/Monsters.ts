a6c80f217e047d8abd3840d1ca278310bade600f
jannik <jannik@nunu.ai>
2025-07-06 10:39:12 +0000

    add monster mapping

---
+++ src/data/wilds/Monsters.ts
@@ -0,0 +1,165 @@
+import type { Monster } from '../../types/Monsters';
+
+import ajarkan from '../../assets/wilds/monsters/ajarkan.png';
+import arkveld from '../../assets/wilds/monsters/arkveld.png';
+import balahara from '../../assets/wilds/monsters/balahara.png';
+import blangonga from '../../assets/wilds/monsters/blangonga.png';
+import chatacabara from '../../assets/wilds/monsters/chatacabara.png';
+import congalala from '../../assets/wilds/monsters/congalala.png';
+import doshaguma from '../../assets/wilds/monsters/doshaguma.png';
+import gore_magala from '../../assets/wilds/monsters/gore_magala.png';
+import gravios from '../../assets/wilds/monsters/gravios.png';
+import guardian_arkveld from '../../assets/wilds/monsters/guardian_arkveld.png';
+import guardian_doshaguma from '../../assets/wilds/monsters/guardian_doshaguma.png';
+import guardian_ebony_odogaron from '../../assets/wilds/monsters/guardian_ebony_odogaron.png';
+import guardian_fulgur_anjanath from '../../assets/wilds/monsters/guardian_fulgur_anjanath.png';
+import guardian_rathalos from '../../assets/wilds/monsters/guardian_rathalos.png';
+import gypceros from '../../assets/wilds/monsters/gypceros.png';
+import hirabami from '../../assets/wilds/monsters/hirabami.png';
+import jhin_dahaad from '../../assets/wilds/monsters/jhin_dahaad.png';
+import lagiacrus from '../../assets/wilds/monsters/lagiacrus.png';
+import lala_barina from '../../assets/wilds/monsters/lala_barina.png';
+import mizutsune from '../../assets/wilds/monsters/mizutsune.png';
+import nerscylla from '../../assets/wilds/monsters/nerscylla.png';
+import nu_udra from '../../assets/wilds/monsters/nu_udra.png';
+import quematrice from '../../assets/wilds/monsters/quematrique.png';
+import rathalos from '../../assets/wilds/monsters/rathalos.png';
+import rathian from '../../assets/wilds/monsters/rathian.png';
+import rey_dau from '../../assets/wilds/monsters/rey_dau.png';
+import rompopolo from '../../assets/wilds/monsters/rompopolo.png';
+import seregios from '../../assets/wilds/monsters/seregios.png';
+import uth_duna from '../../assets/wilds/monsters/uth_dunat.png';
+import xu_wu from '../../assets/wilds/monsters/xu_wu.png';
+import yian_kut_ku from '../../assets/wilds/monsters/yian_kut_kuku.png';
+import zoh_shia from '../../assets/wilds/monsters/zoh_shia.png';
+
+export const wildsMonsters: Monster[] = [
+  {
+    name: 'ajarkan',
+    image: ajarkan,
+  },
+  {
+    name: 'arkveld',
+    image: arkveld,
+  },
+  {
+    name: 'balahara',
+    image: balahara,
+  },
+  {
+    name: 'blangonga',
+    image: blangonga,
+  },
+  {
+    name: 'chatacabara',
+    image: chatacabara,
+  },
+  {
+    name: 'congalala',
+    image: congalala,
+  },
+  {
+    name: 'doshaguma',
+    image: doshaguma,
+  },
+  {
+    name: 'gore_magala',
+    image: gore_magala,
+  },
+  {
+    name: 'gravios',
+    image: gravios,
+  },
+  {
+    name: 'guardian_arkveld',
+    image: guardian_arkveld,
+  },
+  {
+    name: 'guardian_doshaguma',
+    image: guardian_doshaguma,
+  },
+  {
+    name: 'guardian_ebony_odogaron',
+    image: guardian_ebony_odogaron,
+  },
+  {
+    name: 'guardian_fulgur_anjanath',
+    image: guardian_fulgur_anjanath,
+  },
+  {
+    name: 'guardian_rathalos',
+    image: guardian_rathalos,
+  },
+  {
+    name: 'gypceros',
+    image: gypceros,
+  },
+  {
+    name: 'hirabami',
+    image: hirabami,
+  },
+  {
+    name: 'jhin_dahaad',
+    image: jhin_dahaad,
+  },
+  {
+    name: 'lagiacrus',
+    image: lagiacrus,
+  },
+  {
+    name: 'lala_barina',
+    image: lala_barina,
+  },
+  {
+    name: 'mizutsune',
+    image: mizutsune,
+  },
+  {
+    name: 'nerscylla',
+    image: nerscylla,
+  },
+  {
+    name: 'nu_udra',
+    image: nu_udra,
+  },
+  {
+    name: 'quematrice',
+    image: quematrice,
+  },
+  {
+    name: 'rathalos',
+    image: rathalos,
+  },
+  {
+    name: 'rathian',
+    image: rathian,
+  },
+  {
+    name: 'rey_dau',
+    image: rey_dau,
+  },
+  {
+    name: 'rompopolo',
+    image: rompopolo,
+  },
+  {
+    name: 'seregios',
+    image: seregios,
+  },
+  {
+    name: 'uth_duna',
+    image: uth_duna,
+  },
+  {
+    name: 'xu_wu',
+    image: xu_wu,
+  },
+  {
+    name: 'yian_kut_ku',
+    image: yian_kut_ku,
+  },
+  {
+    name: 'zoh_shia',
+    image: zoh_shia,
+  },
+];
