f8233a5d5a1ac48f21b7855af5421692d277a0df
jannik <gartmann.jannik@gmail.com>
2025-08-02 20:41:09 +0000

    wilds guard up page

---
+++ src/data/wilds/GuardUp.ts
@@ -0,0 +1,57 @@
+import type { GuardUp } from '../../types/Monsters';
+
+export const wildsGuardUp: GuardUp[] = [
+  {
+    name: 'jhin dahaad',
+    trueUnblockable: ['Ice nova where he curls up. Can counter charge it though'],
+  },
+  {
+    name: 'zoh shia',
+    guardUp: ['downward fireball'],
+    trueUnblockable: ['all scripted nova attacks'],
+  },
+  {
+    name: 'arkveld',
+    guardUp: ['tail grab attack'],
+  },
+  {
+    name: 'guardian arkveld',
+    guardUp: ['tail grab attack'],
+  },
+  {
+    name: 'rathalos',
+    guardUp: ['pin attack'],
+  },
+  {
+    name: 'guardian rathalos',
+    guardUp: ['pin attack'],
+  },
+  {
+    name: 'seregios',
+    guardUp: ['flying grab attack (pin)'],
+  },
+  {
+    name: 'gravios',
+    guardUp: ['fire gas', 'sleep gas'],
+  },
+  {
+    name: 'guardian fulgur anjanath',
+    guardUp: ['grab attack'],
+  },
+  {
+    name: 'nu udra',
+    guardUp: ['grab attack'],
+  },
+  {
+    name: 'xu wu',
+    guardUp: ['grab attack'],
+  },
+  {
+    name: 'ajarakan',
+    guardUp: ['grab attack'],
+  },
+  {
+    name: 'uth duna',
+    guardUp: ['the big wave after the diving sequence (area 17)'],
+  },
+];
--- src/data/world/GuardUp.ts
+++ src/data/world/GuardUp.ts
@@ -4,178 +4,178 @@ export const worldGuardUp: GuardUp[] = [
   {
     name: 'alatreon',
     guardUp: [
-      'Fire/ice cone spray when on ground',
-      'Fire/ice spray when in the air',
-      'Flamethrower sweep when grounded',
+      'fire/ice cone spray when on ground',
+      'fire/ice spray when in the air',
+      'flamethrower sweep when grounded',
     ],
     trueUnblockable: [
-      "Escation judgement can't be blocked, you need to pass the elemental check to be able to survive it",
+      "escation judgement can't be blocked, you need to pass the elemental check to be able to survive it",
     ],
   },
   {
     name: 'ancient leshen',
-    guardUp: ['The pin attack'],
+    guardUp: ['pin attack'],
     trueUnblockable: [
-      "His super aoe move where the camera zooms out can't be blocked, just sheathe and flash it",
+      "super aoe move where the camera zooms out can't be blocked, just sheathe and flash it",
     ],
   },
   {
     name: 'anjanath',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'azure rathalos',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'behemoth',
-    guardUp: ['Its lightning attacks', 'The fire pillars', 'The belly flop'],
+    guardUp: ['all lightning attacks', 'fire pillars', 'belly flop'],
     trueUnblockable: [
-      "Ecliptic meteor can't be blocked, you need to hide behind a comet to survive it",
-      "The gore quick bite where he grabs you afterwards can't be blocked",
+      "ecliptic meteor can't be blocked, you need to hide behind a comet to survive it",
+      "gore quick bite where he grabs you afterwards can't be blocked",
     ],
   },
   {
     name: 'black diablos',
-    guardUp: ['Small roar into rush attack'],
+    guardUp: ['small roar into rush attack'],
   },
   {
     name: 'blackveil vaal hazak',
-    guardUp: ['All 3 miasma laser attacks', 'Miasma downwards spray'],
+    guardUp: ['all 3 miasma laser attacks', 'miasma downwards spray'],
   },
   {
     name: 'deviljho',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'fatalis',
     guardUp: [
-      'All fireball attacks',
-      'Side to side spray',
+      'all fireball attacks',
+      'side to side spray',
       '360 fire spray',
-      'Fire spray from above',
+      'fire spray from above',
     ],
     trueUnblockable: [
-      "The pin attack in third phase can't be blocked",
-      "Phase transitions can't be blocked, need to hide to survive them",
+      "the pin attack in third phase can't be blocked",
+      "phase transitions can't be blocked, need to hide to survive them",
     ],
   },
   {
     name: 'frostfang barioth',
     trueUnblockable: [
-      "All attacks that leave ice on the ground can't be blocked",
-      "Can't walk through the ice",
+      "all attacks that leave ice on the ground can't be blocked",
+      "can't walk through the ice",
     ],
   },
   {
     name: 'furious rajang',
-    guardUp: ['Its beam attack', 'The wall beam'],
+    guardUp: ['beam attack', 'wall beam'],
   },
   {
     name: 'kirin',
-    guardUp: ['The attack where multiple lightning lines form on the ground'],
+    guardUp: ['attack where multiple lightning lines form on the ground'],
   },
   {
     name: 'kulve taroth',
-    guardUp: ['Fire spray hits'],
+    guardUp: ['fire spray hits'],
   },
   {
     name: 'kushala daora',
-    guardUp: ['Charged wind blast when on the ground'],
+    guardUp: ['charged wind blast when on the ground'],
   },
   {
     name: 'leshen',
-    guardUp: ['The pin attack'],
+    guardUp: ['pin attack'],
     trueUnblockable: [
-      "His super aoe move where the camera zooms out can't be blocked, just sheathe and flash it",
+      "super aoe move where the camera zooms out can't be blocked, just sheathe and flash it",
     ],
   },
   {
     name: 'lunastra',
-    guardUp: ['All attacks that detonate the fire on the ground'],
+    guardUp: ['all attacks that detonate the fire on the ground'],
     trueUnblockable: [
-      'You can only block the wind pressure of the nova with guard up, you will still take damage over time',
+      'you can only block the wind pressure of the nova with guard up, you will still take damage over time',
     ],
   },
   {
     name: 'namielle',
-    guardUp: ['Wave attack, but does a lot of chip'],
-    trueUnblockable: ["The pull that trips the hunter can't be blocked"],
+    guardUp: ['wave attack, but does a lot of chip'],
+    trueUnblockable: ["the pull that trips the hunter can't be blocked"],
   },
   {
     name: 'radobaan',
-    guardUp: ['The sleep gas'],
+    guardUp: ['the sleep gas'],
   },
   {
     name: 'raging brachydios',
-    guardUp: ['The move in last phase where he explodes all slime on the ground'],
+    guardUp: ['the move in last phase where he explodes all slime on the ground'],
   },
   {
     name: 'rajang',
-    guardUp: ['Its beam attack'],
+    guardUp: ['beam attack'],
   },
   {
     name: 'rathalos',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'safi jiiva',
-    guardUp: ['All lasers'],
-    trueUnblockable: ['The nova, just hide behind the rocks'],
+    guardUp: ['all lasers'],
+    trueUnblockable: ['the nova, just hide behind the rocks'],
   },
   {
     name: 'savage deviljho',
-    guardUp: ['Dragon element explosion attack'],
+    guardUp: ['dragon element explosion attack'],
     trueUnblockable: [
-      "Its pin attack can't be blocked",
-      "The attack where he sprays down dragon element can't be blocked",
+      "pin attack can't be blocked",
+      "attack where he sprays down dragon element can't be blocked",
     ],
   },
   {
     name: 'seething bazelgeuse',
-    guardUp: ['Special nuke attack'],
+    guardUp: ['special nuke attack'],
   },
   {
     name: 'shara ishvalda',
-    guardUp: ['All wind laser attacks, the big beam will most likely chip you to death'],
+    guardUp: ['all wind laser attacks, the big beam will most likely chip you to death'],
     trueUnblockable: [
       "The big energy ball move can't be blocked, just run to the side of the arena",
     ],
   },
   {
     name: 'silver rathalos',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'stygian zinogre',
-    guardUp: ['Its pin attack'],
+    guardUp: ['pin attack'],
   },
   {
     name: 'teostra',
-    guardUp: ['Its super nova', 'The flame shower'],
+    guardUp: ['super nova', 'flame shower'],
   },
   {
     name: 'uragaan',
-    guardUp: ['All gas attacks'],
+    guardUp: ['all gas attacks'],
   },
   {
     name: 'vaal hazak',
-    guardUp: ['The miasma laser', 'Miasma downwards spray'],
+    guardUp: ['miasma laser', 'miasma downwards spray'],
   },
   {
     name: 'velkhana',
-    guardUp: ['All 4 ice beams'],
+    guardUp: ['all 4 ice beams'],
     trueUnblockable: [
-      "The freeze breath that leaves a circle on the ground can't be blocked",
-      "The AT velkhanas 360 nova beam can't be blocked",
+      "the freeze breath that leaves a circle on the ground can't be blocked",
+      "the AT velkhana's 360 nova beam can't be blocked",
     ],
   },
   {
     name: 'xeno jiiva',
-    guardUp: ['All energy beams', 'All ground explosion'],
+    guardUp: ['all energy beams', 'all ground explosion'],
   },
   {
     name: 'zinogre',
-    trueUnblockable: ['The pin attack is unblockable'],
+    trueUnblockable: ['pin attack is true unblockable'],
   },
 ];
